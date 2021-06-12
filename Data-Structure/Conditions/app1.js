538b9a42708ea8f7ac484aa66e843b5f79eb6047
naima-shk <naimashaikh550@gmail.com>
2021-06-12 13:46:00 +0500

    'completed-problem-1-using-if-conditions'

--- Data-Structure/Conditions/app1.js
+++ Data-Structure/Conditions/app1.js
@@ -2,10 +2,12 @@
 Consider the situation in which you are required to check if a number is 1 or 0. If it’s 1 you need to print true. Otherwise, you should print false.
 
 */
-let num;
 
-if (num === 1 ){
-    console.log(true);
-} else (num === 0){
-    console.log(false);
+function checkNum() {
+  let num = document.querySelector("input").value;
+  if (num == 1) {
+    console.log("true");
+  } else {
+    console.log("false");
+  }
 }
--- Data-Structure/Conditions/app2.js
+++ Data-Structure/Conditions/app2.js
@@ -2,3 +2,17 @@
 You are given two numbers, num1 = 4 and num2 = 7. Add both and return true if both are equal to 11. Otherwise, return false, if they don’t equal 11.
 
 */
+
+let num1 =4;
+let num2= 7;
+
+sum = num1 + num2; // add the numbers
+
+if(sum == 11){
+   console.log('true');
+} else {
+console.log( 'false');
+}
+
+
+
