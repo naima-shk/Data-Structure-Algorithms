a615acc09bb6c252e9afd65f840077216ab4102d
naima-shk <naimashaikh550@gmail.com>
2021-06-12 14:48:38 +0500

    'folder-structure-reArrange'

--- Conditionals/Switch/app.js
+++ Conditionals/Switch/app.js
@@ -1,15 +1,13 @@
 //Consider the situation in which you are required to check if a number is 1 or 0. If it’s 1 you need to print true. Otherwise, you should print false.
-let numb=1;
+let numb = 1;
 
-
-switch(numb) {
-    case 0:
-     console.log('false');
-      break;
-    case 1:
-      console.log('true');
-      break;
-      default:
-        console.log("No value found");
-  
-  }
+switch (numb) {
+  case 0:
+    console.log("false");
+    break;
+  case 1:
+    console.log("true");
+    break;
+  default:
+    console.log("No value found");
+}
