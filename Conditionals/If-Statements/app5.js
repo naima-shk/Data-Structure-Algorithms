c32acc5d3baeaa4c79c3301b687847b76e5ad792
naima-shk <naimashaikh550@gmail.com>
2021-06-13 13:49:11 +0500

    'problems-implemented-in-if-else'

--- Conditionals/If-Statements/app5.js
+++ Conditionals/If-Statements/app5.js
@@ -1,14 +1,11 @@
 //You need to write a function that prints out ‘wild animals’ if the names of lion and leopard are passed to it. However, if I pass the names of cats and rabbits to it, it prints ‘pet animals’. However, if I pass any other name to it, it says ‘unknown’.
 
- let animals= 'lion';
- 
-    if ( animals == 'lion'  || animals == 'leopard' ){
-        console.log('wild animals')
-    } else if( animals == 'cats'  || animals == 'rabbits'){
-        console.log('pet animals')
-    } else{
-        console.log('unknown')
-    }
+let animals = "lion";
 
-
-
+if (animals == "lion" || animals == "leopard") {
+  console.log("wild animals");
+} else if (animals == "cats" || animals == "rabbits") {
+  console.log("pet animals");
+} else {
+  console.log("unknown");
+}
