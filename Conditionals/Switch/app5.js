cd5597cd2cb1bf328ef3d5cf10dd753e949daff2
naima-shk <naimashaikh550@gmail.com>
2021-06-13 13:47:51 +0500

    'problems-implemented-in-switch'

--- Conditionals/Switch/app5.js
+++ Conditionals/Switch/app5.js
@@ -1,19 +1,17 @@
 // to write a function that prints out ‘wild animals’ if the names of lion and leopard are passed to it. However, if I pass the names of cats and rabbits to it, it prints ‘pet animals’. However, if I pass any other name to it, it says ‘unknown’
-let myAnimals ;
+let myAnimals= 'rabbits';
 
 switch (myAnimals) {
   case 'lion':
+      case 'leopard':
     console.log("wild animals");
     break;
-  case 'leopard':
-    console.log("wild animals");
-    break;
+  
     case 'cats' :
+    case 'rabbit':
     console.log("pet animals");
     break;
-  case 'rabbit':
-    console.log("pet animals");
-    break;
+  
   default:
     console.log("No animal found");
 }
