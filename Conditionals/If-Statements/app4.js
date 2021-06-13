07b2d84f2e4f15580e705c0541c19bc7da5157bd
naima-shk <naimashaikh550@gmail.com>
2021-06-13 13:48:37 +0500

    'problems-implemented-in-id-else'

---
+++ Conditionals/If-Statements/app4.js
@@ -0,0 +1,12 @@
+//Suppose you are given a number and you need to check whether it’s even or odd. How'd you do that?
+let  number=4 ;
+
+//check if the number is even
+if(number % 2 == 0) {
+    console.log("The number is even.");
+}
+
+// if the number is odd
+else {
+    console.log("The number is odd.");
+}
