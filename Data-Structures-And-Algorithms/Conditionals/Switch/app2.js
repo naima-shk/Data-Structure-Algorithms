a65e13bf2af4ccc9d42435bcdd04a39dd2ad620f
naima-shk <naimashaikh550@gmail.com>
2021-06-14 18:08:39 +0500

    'problems-implemented-in-switch'

--- Data-Structures-And-Algorithms/Conditionals/Switch/app2.js
+++ Data-Structures-And-Algorithms/Conditionals/Switch/app2.js
@@ -1,14 +1,14 @@
 //You are given two numbers, num1 = 4 and num2 = 7. Add both and return true if both are equal to 11. Otherwise, return false, if they don’t equal 11.
 let num1 = 4;
-let num2 = 6;
+let num2 = 7;
 
 sum = num1 + num2; // add the numbers
 
-switch (sum) {
-  case 10:
+switch (true) {
+  case (sum >11):
     console.log("false");
     break;
-  case 11:
+  case (sum == 11):
     console.log("true");
     break;
   default:
