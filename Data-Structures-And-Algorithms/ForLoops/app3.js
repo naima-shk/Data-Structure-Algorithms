97718dd14499792f9690d3f2206ee3bd378de07d
naima-shk <naimashaikh550@gmail.com>
2021-06-15 18:40:50 +0500

    'problems-implemented-in-for-loops'

--- Data-Structures-And-Algorithms/ForLoops/app3.js
+++ Data-Structures-And-Algorithms/ForLoops/app3.js
@@ -1,8 +1,12 @@
 //Work on the problem above to dynamically print the name of each animal and prepend it with ‘this is’ conditionally. Print ‘something else’ if the name doesn’t equal any of the names in the list.
 let animals= ['cat', 'rabbit', 'pigeon', 'parrot', 'goldfish'];
 for (let i=0; i<=animals.length; i++){
+    if(animals [i]  === 'cat'){
  console.log('this is ' + animals[i]);
-    } 
+ break;
+    } else{
        { console.log(' this is something else');
-    
+       }
+    }
 }
+
