e60f09499574c81b1ed7ed29433c0617d23fabeb
naima-shk <naimashaikh550@gmail.com>
2021-06-17 18:30:34 +0500

    'to--check-prime-numbers'

--- Data-Structures-And-Algorithms/ForLoops/app1.js
+++ Data-Structures-And-Algorithms/ForLoops/app1.js
@@ -1,14 +1,28 @@
 //Suppose you are given an array: [1, 8, 7, 3, 2, 9, 10, 12]. You need to find out whether a number is prime or not (Hint: a prime number is a number that is divisible by 1 and itself). What approach would you take to do that? Support it with the relevant code.
 // program to check if a number is prime or not
 
-let number = [1, 8, 7, 3, 2, 9, 10, 12];
-
-number = number.filter((myArray) => {
-  for (let i = 2; i <= Math.sqrt(myArray); i++) {
-    if (myArray % i === 0) {
-      return false;
+function checkPrime(number) {
+  if (number <= 1) {
+    return false;
+  } else {
+    for (let i = 2; i < number; i++) {
+      if (number % i == 0) {
+        return false;
+      }
     }
     return true;
   }
+  
+}
+console.log(checkPrime(10))
+
+//let arr = [1, 8, 7, 3, 2, 9, 10, 12];
+/*arr.forEach(function  (element) {
+  let  isPrime = checkPrime(element);
+  if (isPrime) {
+    console.log(`${element} is a prime number`);
+  } else {
+    console.log(`${element} is NOT a prime number`);
+  }
 });
-console.log(number);
+*/
--- Data-Structures-And-Algorithms/ForLoops/app3.js
+++ Data-Structures-And-Algorithms/ForLoops/app3.js
@@ -1,7 +1,7 @@
 //Work on the problem above to dynamically print the name of each animal and prepend it with ‘this is’ conditionally. Print ‘something else’ if the name doesn’t equal any of the names in the list.
 let animals= ['cat', 'rabbit', 'pigeon', 'parrot', 'goldfish'];
 for (let i=0; i<=animals.length; i++){
-    if(animals [i]  === 'cat'){
+    if(animals [i]  === ''){
  console.log('this is ' + animals[i]);
  break;
     } else{
@@ -10,3 +10,13 @@ for (let i=0; i<=animals.length; i++){
     }
 }
 
+/*let animals= ['cat', 'rabbit', 'pigeon', 'parrot', 'goldfish'];
+let  i = 0 ;
+do {
+    console.log("this is ", animals[i]);
+    i++ 
+} while(animals[i] != undefined);
+if(i>4) {
+console.log("this is something else");
+}
+*/
