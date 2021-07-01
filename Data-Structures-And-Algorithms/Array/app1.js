995d10c1c90427d06592b1626be7067a167bbad1
naima-shk <naimashaikh550@gmail.com>
2021-07-01 13:18:03 +0500

    'problems-implemented-in-array'

---
+++ Data-Structures-And-Algorithms/Array/app1.js
@@ -0,0 +1,19 @@
+//Traverse an array [4, -20, 50, 1, 0, -5, 6, 7, 10] and print all of its elements and indices.
+ /*let arr= [4, -20, 50, 1, 0, -5, 6, 7, 10];
+ let a= arr.indexOf();
+ for (let i=0; i<=arr.length; i++){
+     console.log(arr[i]);
+ } */
+
+ /* while loop */
+ let arr= [4, -20, 50, 1, 0, -5, 6, 7, 10];
+ let i=0;
+ let index;
+ while (arr[i]){
+     index += arr[i];
+    
+     console.log(arr);
+     i++;
+     
+ }
+ 
---
+++ Data-Structures-And-Algorithms/Array/app2.js
@@ -0,0 +1,9 @@
+//Suppose you are given an array: [9, 7, 75, 32, 3234, 5, 3] . Find whether 3 is present in the given array. If it’s there return true otherwise return false
+let arr= [9, 7, 75, 32, 3234, 5, 3];
+    
+    // Check if 3  exists in the  array
+    if(arr.indexOf(3) !== -1){
+        console.log("Value exists!")
+    } else{
+        console.log("Value does not exists!")
+    }
