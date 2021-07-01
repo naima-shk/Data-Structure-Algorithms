4cfca60bcb9d68758f7ec01d9f3733c522b872b8
naima-shk <naimashaikh550@gmail.com>
2021-07-01 18:59:06 +0500

    'problems-implemented-in-strings'

---
+++ Data-Structures-And-Algorithms/Strings/app2.js
@@ -0,0 +1,5 @@
+//Take any two random strings and concatenate them to each other.
+let str1= 'naima';
+let str2= 'shaikh';
+let result = str1.concat(str2);
+console.log(result);
---
+++ Data-Structures-And-Algorithms/Strings/app5.js
@@ -0,0 +1,5 @@
+//There’s a string that’s given to you: ‘Reactj’. You need to add s to its last index to make it a complete name i.e. ‘Reactjs’
+let str = 'Reactj';
+let str1= 's';
+let result= str.concat(str1);
+console.log(result);
