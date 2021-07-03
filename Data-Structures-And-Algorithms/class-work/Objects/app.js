e4c48aa58622f931dc01f492810f77855d689ffb
naima-shk <naimashaikh550@gmail.com>
2021-07-03 12:03:24 +0500

    'problems-implemented-in-objects'

--- Data-Structures-And-Algorithms/class-work/Objects/app.js
+++ Data-Structures-And-Algorithms/class-work/Objects/app.js
@@ -1,8 +1,21 @@
 let  myJSON = {
     name:"naima",
      age:25,
-     car: 'BEng'
+     education: 'BEng'
     };
+    //traverse
 for ( let name in myJSON){
     console.log(name+ ": "+ myJSON[name] )
 }
+//access
+console.log(myJSON.age);
+//insertion
+myJSON.location= 'karachi';
+
+console.log( myJSON) 
+//updation
+myJSON.location= 'lahore';
+console.log( myJSON);
+//delete
+delete myJSON.location;
+console.log( myJSON);
