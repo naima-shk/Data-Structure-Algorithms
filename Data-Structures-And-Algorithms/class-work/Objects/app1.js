8d2637d9715f774f32c4d9bc7e22c965675f1623
naima-shk <naimashaikh550@gmail.com>
2021-07-04 11:17:52 +0500

    'problems-implemented-in-objects'

--- Data-Structures-And-Algorithms/class-work/Objects/app1.js
+++ Data-Structures-And-Algorithms/class-work/Objects/app1.js
@@ -40,8 +40,8 @@ className = userObject.class;
 
        //Take the above array of objects and add a book of your choice to it with the same properties.
 
-       
-       function customInsertAtTheEnd(name){
+       //push algorithm
+      /* function customInsertAtTheEnd(name){
            books.length = books.length+1;
            books[books.length-1] = {name: 'the power of your sub conscious mind',
            topics :['self help']
@@ -51,3 +51,17 @@ className = userObject.class;
        
        customInsertAtTheEnd(books);
        console.log(books);
+       */
+      //unshift algorithm
+      function insertAtStart(name){
+        books.length= books.length+1;
+        for(let i=books.length-1; i> 0; i--){
+            books[i]=books[i-1];
+        }
+        books[0]= {name: 'the power of your sub conscious mind',
+        topics :['self help']
+    };
+    
+      }
+      insertAtStart(books);
+        console.log(books);
