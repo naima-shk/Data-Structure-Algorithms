eb1cb79fdf5a811a0cca2c0961be962742b8cb1e
naima-shk <naimashaikh550@gmail.com>
2021-07-08 18:36:49 +0500

    'probelms-implemented-in-stacks'

---
+++ Data-Structures-And-Algorithms/Stack/app1.js
@@ -0,0 +1,48 @@
+//Implement following methods. Don’t forget to increase and decrease the size when you add or remove an element from the stack.
+//push: a method through that you can add a value to the stack
+//peek: a method through which you can get the value on the top of stack
+//pop: a method through which you can remove the first element of the stack.
+
+class Stack {
+    //// Array is used to implement stack
+  constructor() {
+    this.items = [];
+    this.size =0;
+  }
+  // add element to top of stack
+  push(element){
+      this.items[this.size] =element;
+      console.log(`${element} added to ${this.size}`)
+      this.size +=1;
+      return this.size -1;
+  }
+  // return and remove top element in stack
+  // return undefined if stack is empty
+  pop(){
+      if(this.size == 0) return undefined
+      let deleteItem =this.items[this.size -1]
+      this.size -= 1;
+      console.log(`${deleteItem} removed `)
+      return deleteItem;
+  }
+  // check top element in stack
+  peek(){
+      console.log(`Top element is ${this.items[this.size -1]}`)
+      return this.items[this.size -1 ]
+  }
+ 
+    
+}
+
+
+  let myStack = new  Stack()
+ 
+  myStack.push(100)
+  myStack.push(200)
+ 
+  myStack.push(300)
+  myStack.peek()
+  myStack.pop()
+ 
+  myStack.pop()
+  
---
+++ Data-Structures-And-Algorithms/Stack/app3.js
@@ -0,0 +1,25 @@
+//Implement following methods:
+//clear: a method that clears stack
+//toString: a method that converts all members of stack into string
+
+
+class Stack {
+    //// Array is used to implement stack
+  constructor() {
+    this.items = [1,2,3,4,5];
+    this.size =0;
+  }
+
+
+//clear stack
+clear(){
+  this.items=[]
+  this.size =0;
+  console.log('stack cleared..')
+  return this.items
+}
+}
+
+let myStack = new  Stack()
+  console.log(items.toString());
+  //myStack.clear()
